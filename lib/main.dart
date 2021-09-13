b604605ac26dbd289a1b2dc363705d3c0027aa96
chip_pickering <mykeyworks.2113@gmail.com>
2021-09-13 10:19:03 -0700

    hopefully adds ios stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:demo_gram/screens/splash.dart';
 import 'package:demo_gram/state/app_state.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
